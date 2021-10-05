d9347a376c0247f5c1d604264b68efa03b0b0595
myeonghan-nim <ataraxia9408@gmail.com>
2021-10-05 23:51:30 +0900

    Fix: post logic

--- post/views.py
+++ post/views.py
@@ -1,73 +1,44 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
-from account.models import User
 
+from account.models import User
 from .forms import PostForm, CommentForm
 from .models import Post, HashTag, Comment
 
-from django.contrib.auth.decorators import login_required
-
-from django.db.models import Q
-
-from django.core.paginator import Paginator
-
-# Create your views here.
-
 
 def index(request):
-
-    posts = Post.objects.all()
-    comments = Comment.objects.all()
-
-    comment_form = CommentForm()
-
-    paginator = Paginator(posts, 6)
-
-    page = request.GET.get('page')
-    posts = paginator.get_page(page)
-
     context = {
-        'posts': posts,
-        'comments': comments,
-        'comment_form': comment_form,
+        'posts': Paginator(Post.objects.all(), 6).get_page(request.GET.get('page')),
+        'comments': Comment.objects.all(),
+        'comment_form': CommentForm(),
     }
-
     return render(request, 'post/index.html', context)
 
 
 def hashtags(request, hashtag_id):
-
     hashtag = get_object_or_404(HashTag, id=hashtag_id)
-    posts = hashtag.taged_post.all()
-    comments = Comment.objects.all()
-
-    comment_form = CommentForm()
-
     context = {
-        'posts': posts,
-        'comments': comments,
-        'comment_form': comment_form,
+        'posts': hashtag.taged_post.all(),
+        'comments': Comment.objects.all(),
+        'comment_form': CommentForm(),
     }
-
     return render(request, 'post/index.html', context)
 
 
 @login_required
 def create(request):
-
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
-
         if form.is_valid():
-
             post = form.save(commit=False)
             post.user = request.user
             post.save()
-
             for word in post.content.split():
                 if word.startswith('#'):
-                    hashtag = HashTag.objects.get_or_create(content=word)[0]
-                    post.hashtags.add(hashtag)
-
+                    post.hashtags.add(
+                        HashTag.objects.get_or_create(content=word)[0])
             return redirect('post:index')
     else:
         form = PostForm()
@@ -76,29 +47,22 @@ def create(request):
         'form': form,
     }
 
-    return render(request, 'post/forms.html', context)
+    return render(request, 'post/form.html', context)
 
 
 @login_required
 def update(request, post_id):
-
     post = get_object_or_404(Post, id=post_id)
-
     if post.user == request.user:
         if request.method == 'POST':
-            form = PostForm(request.POST, instance=post)
-
+            form = PostForm(request.POST, request.FILES, instance=post)
             if form.is_valid():
-
                 post = form.save()
                 post.hashtags.clear()
-
                 for word in post.content.split():
                     if word.startswith('#'):
-                        hashtag = HashTag.objects.get_or_create(content=word)[
-                            0]
-                        post.hashtags.add(hashtag)
-
+                        post.hashtags.add(
+                            HashTag.objects.get_or_create(content=word)[0])
                 return redirect('post:index')
         else:
             form = PostForm(instance=post)
@@ -107,25 +71,21 @@ def update(request, post_id):
             'form': form,
         }
 
-        return render(request, 'post/forms.html', context)
+        return render(request, 'post/form.html', context)
 
     return redirect('post:index')
 
 
 @login_required
 def delete(request, post_id):
-
     post = get_object_or_404(Post, id=post_id)
-
     if post.user == request.user:
         post.delete()
-
     return redirect('post:index')
 
 
 @login_required
 def like(request, post_id):
-
     post = get_object_or_404(Post, id=post_id)
     user = request.user
 
@@ -138,12 +98,10 @@ def like(request, post_id):
 
 
 def search(request):
-
-    target = request.GET.get('search')
-    posts = Post.objects.filter(Q(content__contains=target))
-
     context = {
-        'posts': posts,
+        'posts': Paginator(
+            Post.objects.filter(Q(content__contains=request.GET.get('search'))), 6).get_page(request.GET.get('page')),
+        'comments': Comment.objects.all(),
+        'comment_form': CommentForm(),
     }
-
     return render(request, 'post/index.html', context)
