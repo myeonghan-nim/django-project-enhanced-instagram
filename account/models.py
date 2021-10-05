31bd54e8ac76783eec8d6373e72da5f5d7a3250a
myeonghan-nim <ataraxia9408@gmail.com>
2021-10-05 23:00:58 +0900

    Fix: typo

--- account/models.py
+++ account/models.py
@@ -1,10 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
 
 class User(AbstractUser):
-
-    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followings')
-    
+    followers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='followings')
