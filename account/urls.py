70e71b887d90c44061a5a8555c607988d56127c8
myeonghan-nim <ataraxia9408@gmail.com>
2021-10-05 23:01:09 +0900

    Fix: account logic

--- account/urls.py
+++ account/urls.py
@@ -1,20 +1,18 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-
-    path('update/', views.update, name='update'),
-    path('password/', views.password, name='password'),
-
-    path('<int:user_id>/delete/', views.delete, name='delete'),
-
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 
+    path('update/', views.update, name='update'),
+    path('password/', views.password, name='password'),
+    path('<int:user_id>/delete/', views.delete, name='delete'),
+
     path('<int:user_id>/', views.userpage, name='userpage'),
-    path('profile/', views.profile, name='profile'),
     path('<int:user_id>/follow/', views.follow, name='follow'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,26 +1,19 @@
-from django.shortcuts import render, redirect, get_object_or_404
-
-from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, PasswordChangeForm
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
 
 def signup(request):
-
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
-
         if form.is_valid():
             form.save()
-
             return redirect('account:login')
     else:
         form = CustomUserCreationForm()
@@ -33,13 +26,10 @@ def signup(request):
 
 
 def update(request):
-
     if request.method == 'POST':
         form = CustomUserChangeForm(request.POST, instance=request.user)
-
         if form.is_valid():
             form.save()
-
             return redirect('post:index')
     else:
         form = CustomUserChangeForm(instance=request.user)
@@ -52,14 +42,11 @@ def update(request):
 
 
 def password(request):
-
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
-
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, form.user)
-
             return redirect('post:index')
     else:
         form = PasswordChangeForm(request.user)
@@ -72,24 +59,16 @@ def password(request):
 
 
 def delete(request, user_id):
-
-    user_info = get_object_or_404(User, id=user_id)
-    user = request.user
-
-    if user == user_info:
-        user.delete()
-
+    if request.user == get_object_or_404(User, id=user_id):
+        request.user.delete()
     return redirect('post:index')
 
 
 def login(request):
-
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
-
         if form.is_valid():
             auth_login(request, form.get_user())
-
             return redirect('post:index')
     else:
         form = AuthenticationForm()
@@ -102,44 +81,26 @@ def login(request):
 
 
 def logout(request):
-
     auth_logout(request)
-
     return redirect('post:index')
 
 
 def userpage(request, user_id):
-
-    user_info = get_object_or_404(User, id=user_id)
-
     context = {
-        'user_info': user_info,
+        'user_info': get_object_or_404(User, id=user_id),
     }
-
-    return render(request, 'account/userpage.html', context)
-
-
-def profile(request):
-
-    user_info = request.user
-
-    context = {
-        'user_info': user_info,
-    }
-
     return render(request, 'account/userpage.html', context)
 
 
 @login_required
 def follow(request, user_id):
+    following = get_object_or_404(User, id=user_id)
+    follower = request.user
 
-    you = get_object_or_404(User, id=user_id)
-    me = request.user
-
-    if me != you:
-        if me in you.followers.all():
-            you.followers.remove(me)
+    if follower != following:
+        if follower in following.followers.all():
+            following.followers.remove(follower)
         else:
-            you.followers.add(me)
+            following.followers.add(follower)
 
     return redirect('account:userpage', user_id)
