a386efb88206d22b00e1f77e3913b33267502dea
myeonghan-nim <ataraxia9408@gmail.com>
2021-10-05 23:51:24 +0900

    Fix: typo

--- post/templatetags/make_link.py
+++ post/templatetags/make_link.py
@@ -1,21 +1,16 @@
-from django import template
-
 import re
+
+from django import template
 
 register = template.Library()
 
 
 @register.filter
 def hashtag_link(post):
-
     content = post.content
-    hashtags = post.hashtags.all()
-
-    for hashtag in hashtags:
+    for hashtag in post.hashtags.all():
         content = re.sub(
             fr'{hashtag.content}\b',
             f'<a href="/post/hashtags/{hashtag.id}/">{hashtag.content}</a>',
-            content
-        )
-
+            content)
     return content
